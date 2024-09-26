eae4e44c4b5b3d38bf5a772bc25a179e5a39b9c2
Volleyball Player <olivera.janic@yahoo.com>
2024-09-26 12:39:03 +0200

    Add run behavioral pattern cli

---
+++ docs/source/technical_documentation/autodoc/designpatterns.helpers.rst
@@ -0,0 +1,21 @@
+designpatterns.helpers package
+==============================
+
+Submodules
+----------
+
+designpatterns.helpers.receipts module
+--------------------------------------
+
+.. automodule:: designpatterns.helpers.receipts
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: designpatterns.helpers
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- src/designpatterns/cli/run_behavioral_patterns.py
+++ src/designpatterns/cli/run_behavioral_patterns.py
@@ -6,17 +6,17 @@ from designpatterns.utilities.package_resources import PackageResources
 
 @click.group()
 def cli() -> None:
-    """Run structural patterns."""
+    """Run behavioral patterns."""
     logger.info(f'Using design patterns package version {PackageResources.get_package_version()}')
 
 
 @cli.command()
-def adapter() -> None:
-    """Run adapter example.
+def command() -> None:
+    """Run command example.
 
-    This function calls adapter module to run an example of structural design pattern called adapter.
+    This function calls command module to run an example of behavioral design pattern called command.
     """
-    logger.info('This is adapter pattern')
+    logger.info('This is command pattern')
 
 
 if __name__ == '__main__':
