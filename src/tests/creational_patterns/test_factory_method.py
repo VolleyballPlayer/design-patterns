5a7ae1b8d1c5c92a39e3ac74cb41b9356bcd640c
VolleyballPlayer <149392056+VolleyballPlayer@users.noreply.github.com>
2024-12-23 15:00:12 +0100

    Change client code method name

--- src/tests/creational_patterns/test_factory_method.py
+++ src/tests/creational_patterns/test_factory_method.py
@@ -4,20 +4,20 @@ from designpatterns.creational_patterns.factory_method import (
     CappuccinoCreator,
     EspressoCreator,
     LatteCreator,
-    client_code,
+    client_code_factory_method,
 )
 from designpatterns.helpers.receipts import Receipt, cappuccino_receipt, espresso_receipt, latte_receipt
 
 
 class TestFactoryMethod:
     def test__run_factory_method__prepared_latte(self, caplog: pytest.LogCaptureFixture) -> None:
-        client_code(LatteCreator())
+        client_code_factory_method(LatteCreator())
         assert Receipt.get_coffee(receipt=latte_receipt) in caplog.text
 
     def test__run_factory_method__prepared_cappuccino(self, caplog: pytest.LogCaptureFixture) -> None:
-        client_code(CappuccinoCreator())
+        client_code_factory_method(CappuccinoCreator())
         assert Receipt.get_coffee(cappuccino_receipt) in caplog.text
 
     def test__run_factory_method__prepared_espresso(self, caplog: pytest.LogCaptureFixture) -> None:
-        client_code(EspressoCreator())
+        client_code_factory_method(EspressoCreator())
         assert Receipt.get_coffee(espresso_receipt) in caplog.text
